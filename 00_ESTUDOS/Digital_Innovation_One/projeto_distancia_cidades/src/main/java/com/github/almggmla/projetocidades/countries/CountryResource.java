011ad1e45e2e8158f1c35d5dddea70f624e8084b
AlmggmlA <aprigioluiz@gmail.com>
2021-10-10 16:38:42 -0300

    adicionando os estudos projeto_distancia_cidades

---
+++ 00_ESTUDOS/Digital_Innovation_One/projeto_distancia_cidades/src/main/java/com/github/almggmla/projetocidades/countries/CountryResource.java
@@ -0,0 +1,42 @@
+package com.github.almggmla.projetocidades.countries;
+
+import com.github.almggmla.projetocidades.countries.Country;
+import com.github.almggmla.projetocidades.countries.repository.CountryRepository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countries") //minha rota
+public class CountryResource {
+
+    //@Autowired ---> não precisaria criar o construtor, ele mesmo resolveria isso.
+    private CountryRepository repository;
+
+    public CountryResource(CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+        if (optional.isPresent()){ // verifica se existe um país no ID informado.
+            return ResponseEntity.ok().body(optional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
